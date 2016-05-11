4137bffda0be48095ab6cf9af85f6ce2184d8e14
Alex Chermenin <alex@chermenin.ru>
2016-05-11 17:23:07 +0300

    Added comments by Disqus

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,16 @@ var through     = require('through2');
 var connect     = require('connect');
 var http        = require('http');
 
+String.prototype.hashCode = function() {
+  var hash = 0, i, chr, len;
+  if (this.length === 0) return hash;
+  for (i = 0, len = this.length; i < len; i++) {
+    chr   = this.charCodeAt(i);
+    hash  = ((hash << 5) - hash) + chr;
+    hash |= 0; // Convert to 32bit integer
+  }
+  return hash;
+};
 
 var src = __dirname + '/src';
 var site        = require(src + '/settings/site.json');
